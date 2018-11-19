56685ef9ae37f3c3fcc065bb20d59731487dc370
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-19 12:34:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,8 +57,7 @@ function total() {
 
 function removeFromCart(item) {
   var removedItem = toRemove(item)
-
-  if (removedItem=== item){
+  if (removedItem === item){
     var toRemoveIndex = cart.indexOf(removedItem)
     getCart().splice(toRemoveIndex, 1)
   }
@@ -78,5 +77,13 @@ function toRemove(item){
 
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  else{
+    cart = []
+    var totalCost = total()
+    return `Your total cost is $${totalCost}, whioch will be charged to the card ${cardnumber}.`
+  }
+    
 }
